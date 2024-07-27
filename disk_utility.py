f57cdf88bdb461c3cbf5f380aa8a3d440fcbdf3c
Baibhab Adhikari <jiniaadhikari.resto@gmail.com>
2024-07-27 17:47:28 +0530

    Refactor python script for disk utility

--- disk_utility.py
+++ disk_utility.py
@@ -31,7 +31,8 @@ import os
 import shutil
 import zipfile
 # global const
-BYTES_PER_UNIT = 1024  
+BYTES_PER_UNIT = 1024
+
 
 def converter(size_bytes, choice):
     """
@@ -63,19 +64,22 @@ def converter(size_bytes, choice):
 program_standby = True  # bool flag
 # printing menu options
 while program_standby:
-    
+
     print("Welcome to Disk Utility! What do you want to perform?")
     print("A. Zip Directories / Files\nB. Unzip Files\nC. Analyze Disk Space\nD. Exit")
 
     menu_choice = input("Please enter A,B,C or D: ")
     while menu_choice.strip().lower() not in "abcd":
         menu_choice = input("Please enter the correct option! : ")
-    
+
     # check input scenarios
-    if menu_choice.lower() == 'a': 
-        PATH = input("Please enter the path of your desired directory or file to zip: ")
-        output_name = input("Enter the name for the output zip file (without extension): ")
-        output_directory = input("Enter the directory where you want to save the zip file: ")
+    if menu_choice.lower() == 'a':
+        PATH = input(
+            "Please enter the path of your desired directory or file to zip: ")
+        output_name = input(
+            "Enter the name for the output zip file (without extension): ")
+        output_directory = input(
+            "Enter the directory where you want to save the zip file: ")
         output_path = os.path.join(output_directory, output_name)
 
         if os.path.isdir(PATH):
@@ -89,14 +93,15 @@ while program_standby:
             print("The path provided does not exist or is not valid.")
 
         print(f"Your file / directory is zipped as {output_path}")
-            
+
     elif menu_choice.lower() == 'b':
         # Prompt user for the zip file path
         zip_path = input("Please enter the path of the zip file to unpack: ")
         # Check if the zip file exists
         if os.path.isfile(zip_path) and zip_path.endswith('.zip'):
             # Ask for the destination directory
-            destination = input("Enter the destination directory (leave blank to extract in the current directory): ")
+            destination = input(
+                "Enter the destination directory (leave blank to extract in the current directory): ")
             if not destination:
                 # If no destination is provided, extract in the current directory
                 destination = os.getcwd()
@@ -111,7 +116,8 @@ while program_standby:
 
     elif menu_choice.lower() == "c":
         # prompt user for the directory path
-        PATH = input("Please enter the path of your desired directory to analyze: ")
+        PATH = input(
+            "Please enter the path of your desired directory to analyze: ")
         # check for existence
         if os.path.exists(PATH):
 
@@ -129,7 +135,7 @@ while program_standby:
                     try:
                         file_size = os.path.getsize(file_path)
                     except OSError:
-                        print(f"Could not access file: {file_path}")  
+                        print(f"Could not access file: {file_path}")
                         file_size = 0
                     size_file[file_path] = file_size
                     total_dir_size += file_size
@@ -140,7 +146,8 @@ while program_standby:
             choose = "wrong"
 
             while choose.strip().lower() not in "abcd":
-                choose = input("Please choose the conversion unit : \n'a' for bytes to KB\n'b' for bytes to MB\n'c' for bytes to GB\n'd' for bytes to TB\n")
+                choose = input(
+                    "Please choose the conversion unit : \n'a' for bytes to KB\n'b' for bytes to MB\n'c' for bytes to GB\n'd' for bytes to TB\n")
             print("Converted file sizes in user chosen unit:")
             print("---------------------------------------------------------------------------------------------------------")
             print("Directories: ")
@@ -156,4 +163,3 @@ while program_standby:
         print("Thankyou for using the Disk Utility Tool!\nExiting the program......")
         program_standby = False
         break
-    
